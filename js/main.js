422f26b05f7e3de1ec0ea806c70dcf23d103897c
Daniel Bernal <bernalvalencia10@gmail.com>
2023-09-04 21:28:40 -0500

    svg animation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const elements = document.querySelectorAll('.animate-on-scroll');
 
 function checkScroll() {
@@ -8,9 +9,22 @@ function checkScroll() {
         if (elementTop < windowHeight * 0.8) {
             element.classList.add('animate');
         }
+
+        let path = document.querySelector('path')
+        let pathLength = path.getTotalLength()
+
+        path.style.strokeDasharray = pathLength + ' ' + pathLength;
+        path.style.strokeDashoffset = pathLength;
+
+        var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+
+        var drawLength = pathLength * scrollPercentage
+
+        path.style.strokeDashoffset = pathLength - drawLength
     });
 }
 
 window.addEventListener('scroll', checkScroll);
 window.addEventListener('load', checkScroll);
+
 
